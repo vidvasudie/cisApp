6ce4e7b6ae300c0fc065d54e9900442df66573c6
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2021-11-17 00:30:38 +0700

    เพิ่ม paging ใน itemlist

--- cisApp/Controllers/userApproveController.cs
+++ cisApp/Controllers/userApproveController.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using cisApp.library;
+using cisApp.Core;
+using cisApp.Models;
 
 namespace cisApp.Controllers
 {
     public class userApproveController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        private List<UserModel> _model = new List<UserModel>(){
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+                new UserModel(),
+            };
+        public IActionResult Index(int pageIndex=1)
+        { 
+            return View(new PaginatedList<UserModel>(_model, _model.Count, pageIndex, 2));
         }
+        [HttpPost]
+        public PartialViewResult ItemList(int pageIndex = 1)
+        { 
+            return PartialView("PT/_itemlist", new PaginatedList<UserModel>(_model, _model.Count, pageIndex, 2)); 
+        }
+
         public IActionResult Manage()
         {
             return View();
---
+++ cisApp/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cisApp.Core;
+
+namespace cisApp.Models
+{
+    public class UserModel
+    {
+        Users user { get; set; }
+        UserDesigner designer { get; set; }
+        UsersPassword password { get; set; }
+    }
+}
