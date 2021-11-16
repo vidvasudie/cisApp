4ff2a37041dfa2bd4b969eea691f20b9d1d70806
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2021-11-16 23:01:52 +0700

    new partial user

---
+++ cisApp.library/PaginatedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cisApp.library
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }  
+        public static PaginatedList<T> ToPaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+        //public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        //{
+        //    var count = await source.CountAsync();
+        //    var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+        //    return new PaginatedList<T>(items, pageIndex, pageSize);
+        //}
+    }
+}
