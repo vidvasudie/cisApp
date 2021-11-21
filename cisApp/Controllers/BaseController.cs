78f6ca02660b3f9e32de1f08b5f67dc0ef8457da
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2021-11-21 20:52:52 +0700

    เพิ่มตัว check permission

--- cisApp/Controllers/BaseController.cs
+++ cisApp/Controllers/BaseController.cs
@@ -14,15 +14,33 @@ namespace cisApp.Controllers
     {
 
         public static Guid? _UserId {get; set;}
+        public static Guid? _RoleId { get; set; }
+        public static int? _RoleMenuType { get; set; }
 
-        public class Authorized : ActionFilterAttribute
+        public class CustomActionExecuteAttribute : ActionFilterAttribute
         {
+            private readonly Guid _menuid;
+            public CustomActionExecuteAttribute(string menuId)
+            {
+                // _menuid  คือ  MenuId ของตารางเมนู 
+                _menuid = Guid.Parse(menuId); 
+            }
             public override void OnActionExecuting(ActionExecutingContext context)
             {
 
-                Guid fixGuid = new Guid("F71758CF-3F30-432B-8361-5754CDE8BF26");
+                Guid fixUserGuid = new Guid("F71758CF-3F30-432B-8361-5754CDE8BF26");
+                Guid fixRoleGuid = new Guid("0A36BB48-C69B-4B3B-9E2E-F486918A5352"); //admin
 
-                _UserId = fixGuid;
+                _UserId = fixUserGuid;
+                //var roleId = context.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "RoleId")?.Value;
+                //if (!String.IsNullOrEmpty(roleId))
+                //{
+                //    _RoleId = Guid.Parse(roleId);
+                //}
+                _RoleId = fixRoleGuid;
+                var roleMenu = GetRoleMenu.Get.GetByRoleIdAndMenuId(_RoleId.Value, _menuid);
+                _RoleMenuType = roleMenu.Type;
+
 
                 base.OnActionExecuting(context);
             }
