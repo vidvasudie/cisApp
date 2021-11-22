056af4fc47702298227776c3907e36c035474b35
thitsaran <thitsaran.p@gmail.com>
2021-11-22 23:04:27 +0700

    เพิ่มหน้า ลืมหรัสผ่าน แก้ไขกด login แล้วยิงข้อมูลเบิ้ล 2 ที

--- cisApp.library/SendMail.cs
+++ cisApp.library/SendMail.cs
@@ -15,7 +15,7 @@ namespace cisApp.library
 					  .AddJsonFile("appsettings.json")
 					  .Build();
 
-		public static bool SendMailStatus(string subject, string fullname, string userType, string status, string toMail, string webRootPath, string lang)
+		public static bool SendMailResetPassword(string toMail, string username, string password, string webRootPath)
 		{
 			string host = config.GetSection("ConfigMail:Host").Value;
 			string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
@@ -25,362 +25,31 @@ namespace cisApp.library
 			string domain = config.GetSection("Domain").Value;
 			try
 			{
-				string _filePath = Path.Combine(webRootPath, "Templates/SendUpdateStatusTH.htm");
+				string _filePath = Path.Combine(webRootPath, "Templates/UserForgetPassword.htm");
 				string html = System.IO.File.ReadAllText(_filePath);
 
-				//ใช้ทดสอบ
 				MailMessage mail = new MailMessage();
 				SmtpClient SmtpServer = new SmtpClient();
 				SmtpServer.UseDefaultCredentials = false;
 				mail.To.Add(toMail);
 				mail.From = new MailAddress(fromEmail);
-				mail.Subject = subject;
+				mail.Subject = "คำขอรีเซ็ตรหัสผ่าน";
 				mail.IsBodyHtml = true;
-				mail.Body = string.Format(html, fullname, userType, status, domain + "Login");
+				mail.Body = string.Format(html, username, password);
 				SmtpServer.EnableSsl = enableSsl;
-				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 				SmtpServer.Host = host;
 				SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
 				SmtpServer.Port = port;
 				SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
 				SmtpServer.Send(mail);
 
-
-
-				//MailMessage mailMessage = new MailMessage();
-				//SmtpClient smtpClient = new SmtpClient();
-				//smtpClient.Host = host;
-				//smtpClient.Port = port;
-				//smtpClient.EnableSsl = true;
-				//if (smtpClient.EnableSsl)
-				//{
-				//	ServicePointManager.ServerCertificateValidationCallback =
-				//		delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-				//		{ return true; };
-				//}
-				//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-				//smtpClient.UseDefaultCredentials = false;
-				//smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-				//NetworkCredential MyCredentials = new NetworkCredential(fromEmail, passwordEmail);
-				//smtpClient.Credentials = MyCredentials;
-				//smtpClient.Timeout = 100000;
-				//mailMessage.From = new MailAddress(fromEmail);
-				//mailMessage.Subject = "รหัสผ่านเข้าใช้งานของท่าน";
-				//mailMessage.Body = "ดดด";
-				//mailMessage.IsBodyHtml = true;
-				//if (SetMailAddressCollection(mailMessage.To, toMail))
-				//{
-				//	smtpClient.Send(mailMessage);
-				//}
-				//return true;
-
-
 				return true;
 			}
 			catch (Exception ex)
 			{
-				return false;
+				throw ex;
 			}
 		}
-		public static bool SendMailPassword(string fullname, string username, string password, string toMail, string webRootPath, string lang)
-		{
-			string host = config.GetSection("ConfigMail:Host").Value;
-			string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-			string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-			int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-			bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-			string domain = config.GetSection("Domain").Value;
-			try
-			{
-				string _filePath = Path.Combine(webRootPath, "Templates/SendPasswordTH.htm");
-				string html = System.IO.File.ReadAllText(_filePath);
-
-				//ใช้ทดสอบ
-				MailMessage mail = new MailMessage();
-				SmtpClient SmtpServer = new SmtpClient();
-				SmtpServer.UseDefaultCredentials = false;
-				mail.To.Add(toMail);
-				mail.From = new MailAddress(fromEmail);
-				mail.Subject = "รหัสผ่านของท่าน";
-				mail.IsBodyHtml = true;
-				mail.Body = string.Format(html, fullname, username, password, domain + "Login");
-				SmtpServer.EnableSsl = enableSsl;
-				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-				SmtpServer.Host = host;
-				SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
-				SmtpServer.Port = port;
-				SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-				SmtpServer.Send(mail);
-
-
-
-				//MailMessage mailMessage = new MailMessage();
-				//SmtpClient smtpClient = new SmtpClient();
-				//smtpClient.Host = host;
-				//smtpClient.Port = port;
-				//smtpClient.EnableSsl = true;
-				//if (smtpClient.EnableSsl)
-				//{
-				//	ServicePointManager.ServerCertificateValidationCallback =
-				//		delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-				//		{ return true; };
-				//}
-				//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-				//smtpClient.UseDefaultCredentials = false;
-				//smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-				//NetworkCredential MyCredentials = new NetworkCredential(fromEmail, passwordEmail);
-				//smtpClient.Credentials = MyCredentials;
-				//smtpClient.Timeout = 100000;
-				//mailMessage.From = new MailAddress(fromEmail);
-				//mailMessage.Subject = "รหัสผ่านเข้าใช้งานของท่าน";
-				//mailMessage.Body = "ดดด";
-				//mailMessage.IsBodyHtml = true;
-				//if (SetMailAddressCollection(mailMessage.To, toMail))
-				//{
-				//	smtpClient.Send(mailMessage);
-				//}
-				//return true;
-
-
-				return true;
-			}
-			catch (Exception ex)
-			{
-				return false;
-			}
-		}
-
-		public static bool SendMailResetPassword(string fullname, string username, string password, string toMail, string webRootPath, string lang)
-		{
-			string host = config.GetSection("ConfigMail:Host").Value;
-			string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-			string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-			int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-			bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-			string domain = config.GetSection("Domain").Value;
-			try
-			{
-				string _filePath = Path.Combine(webRootPath, "Templates/ChangePasswordTH.htm");
-				string html = System.IO.File.ReadAllText(_filePath);
-
-				//ใช้ทดสอบ
-				MailMessage mail = new MailMessage();
-				SmtpClient SmtpServer = new SmtpClient();
-				SmtpServer.UseDefaultCredentials = false;
-				mail.To.Add(toMail);
-				mail.From = new MailAddress(fromEmail);
-				mail.Subject = "รหัสผ่านใหม่ของท่าน";
-				mail.IsBodyHtml = true;
-				mail.Body = string.Format(html, fullname, username, password, domain + "Login");
-				SmtpServer.EnableSsl = enableSsl;
-				//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-				SmtpServer.Host = host;
-				SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
-				SmtpServer.Port = port;
-				SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-				SmtpServer.Send(mail);
-				return true;
-			}
-			catch (Exception ex)
-			{
-				return false;
-			}
-		}
-
-		//public static bool SendMailRequestReportStatus(string fullName, string requestCode, string requestDate, string toMail, string webRootPath, string lang)
-		//{
-		//	string host = config.GetSection("ConfigMail:Host").Value;
-		//	string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-		//	string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-		//	int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-		//	bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-		//	string domain = config.GetSection("Domain").Value;
-		//	try
-		//	{
-		//		string _filePath = Path.Combine(webRootPath, "Templates/SendRequestReportStatusTH.htm");
-		//		string html = System.IO.File.ReadAllText(_filePath);
-
-		//		//ใช้ทดสอบ
-		//		MailMessage mail = new MailMessage();
-		//		SmtpClient SmtpServer = new SmtpClient();
-		//		SmtpServer.UseDefaultCredentials = false;
-		//		mail.To.Add(toMail);
-		//		mail.From = new MailAddress(fromEmail);
-		//		mail.Subject = "อัพเดตสถานะการขอใช้รายงาน";
-		//		mail.IsBodyHtml = true;
-		//		mail.Body = string.Format(html, fullName, requestCode, fullName, requestDate, GetReportRows(requestCode), domain + "Login");
-		//		SmtpServer.EnableSsl = enableSsl;
-		//		//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-		//		SmtpServer.Host = host;
-		//		//SmtpServer.UseDefaultCredentials = false;
-		//		SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
-		//		SmtpServer.Port = port;
-		//		SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-		//		SmtpServer.Send(mail);
-
-		//		return true;
-		//	}
-		//	catch (Exception ex)
-		//	{
-		//		Console.WriteLine(ex.ToString());
-		//		return false;
-		//	}
-		//}
-
-		//public static bool SendMailAppointment(SysAppointment sysAppointment, string webRootPath)
-		//{
-		//	string host = config.GetSection("ConfigMail:Host").Value;
-		//	string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-		//	string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-		//	int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-		//	bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-		//	string domain = config.GetSection("Domain").Value;
-		//	try
-		//	{
-		//		SysUser sysUser = GetSysUser.Get.GetByGuid(sysAppointment.Usercode);
-		//		string _filePath = "";
-		//		string html = "";
-		//		string body = "";
-		//		string appointmentDate = sysAppointment.Appmdate.Value.ToString("dd MMM yy", new CultureInfo("th-TH"))
-		//			+ " " + TimeFormat.ToTimeFormat(sysAppointment.Appmtime) + "น.";
-		//		string detailUrl = domain + "AppointmentRequest/Report/" + sysAppointment.Appmcode.Value.ToString();
-		//		string appointmentStatus = "";
-		//		if (sysAppointment.Appmstatus == 1)
-		//		{
-		//			appointmentStatus = "รับนัดหมาย";
-		//		}
-		//		else if (sysAppointment.Appmstatus == 2)
-		//		{
-		//			appointmentStatus = "เลื่อนนัดหมาย";
-		//		}
-		//		else if (sysAppointment.Appmstatus == 3)
-		//		{
-		//			appointmentStatus = "ยกเลิกนัดหมาย";
-		//		}
-
-		//		_filePath = Path.Combine(webRootPath, "Templates/SendAppointmentApprove.htm");
-		//		html = System.IO.File.ReadAllText(_filePath);
-
-		//		body = string.Format(html, sysUser.Firstname + " " + sysUser.Surname, appointmentStatus
-		//			, appointmentDate, sysAppointment.DoctorName, sysAppointment.RespDoctorName
-		//			, sysAppointment.Appmremark, QrGenerator.GetUrl(sysAppointment.Appmcode.Value), detailUrl
-		//			, BarcodeGenerator.GetUrl(sysAppointment.Apcode));
-
-
-		//		string toMail = sysUser.Email;
-		//		string subject = "อัพเดทสถานะการนัดหมาย " + sysAppointment.Apcode;
-
-		//		//ใช้ทดสอบ
-		//		MailMessage mail = new MailMessage();
-		//		SmtpClient SmtpServer = new SmtpClient();
-		//		SmtpServer.UseDefaultCredentials = false;
-		//		mail.To.Add(toMail);
-		//		mail.From = new MailAddress(fromEmail);
-		//		mail.Subject = subject;
-		//		mail.IsBodyHtml = true;
-
-
-		//		mail.Body = body;
-		//		SmtpServer.EnableSsl = enableSsl;
-		//		ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-		//		SmtpServer.Host = host;
-		//		SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
-		//		SmtpServer.Port = port;
-		//		SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-		//		SmtpServer.Send(mail);
-
-		//		return true;
-		//	}
-		//	catch (Exception ex)
-		//	{
-		//		return false;
-		//	}
-		//}
-
-		//private static string GetReportRows(string requestCode)
-		//{
-		//	var maps = GetSysReportReqMap.Get.GetByRequestCode(requestCode);
-		//	string template = "<tr> " +
-		//	 "   <td style=\"text-align: left;padding-left: 10px; \">{0}</td>" +
-		//	 "   <td style=\"text-align: center; \">{1}</td>" +
-		//	 "</tr>";
-		//	string tmp = "";
-		//	foreach (var item in maps)
-		//	{
-		//		tmp += String.Format(template, GetSysReport.Get.GetByReportId(item.Reportcode).ReportName, item.Status == null ? "รอดำเนินการ" : item.Status.Value ? "อนุมัติ" : "ไม่อนุมัติ");
-		//	}
-
-		//	return tmp;
-		//}
-
-		private static bool SetMailAddressCollection(MailAddressCollection toAddresses, string mailId)
-		{
-			bool successfulAddressCreation = true;
-			toAddresses.Add(new MailAddress(mailId));
-			return successfulAddressCreation;
-		}
-
-
-
-		public static bool TestMail(string fullname, string username, string password, string toMail, string webRootPath, string lang)
-		{
-			string host = config.GetSection("ConfigMail:Host").Value;
-			string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-			string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-			int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-			bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-			string domain = config.GetSection("Domain").Value;
-			string _filePath = Path.Combine(webRootPath, "Templates/ChangePasswordTH.htm");
-			string html = System.IO.File.ReadAllText(_filePath);
-
-			//ใช้ทดสอบ
-			MailMessage mail = new MailMessage();
-			SmtpClient SmtpServer = new SmtpClient();
-			SmtpServer.UseDefaultCredentials = false;
-			mail.To.Add(toMail);
-			mail.From = new MailAddress(fromEmail);
-			mail.Subject = "รหัสผ่านใหม่ของท่าน";
-			mail.IsBodyHtml = true;
-			mail.Body = string.Format(html, fullname, username, password, domain + "Login");
-			SmtpServer.EnableSsl = enableSsl;
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-			SmtpServer.Host = host;
-			SmtpServer.Credentials = new System.Net.NetworkCredential(fromEmail, passwordEmail);
-			SmtpServer.Port = port;
-			SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-			SmtpServer.Send(mail);
-			return true;
-		}
-
-
-		public static bool TestMailNotPass(string fullname, string username, string password, string toMail, string webRootPath, string lang)
-		{
-			string host = config.GetSection("ConfigMail:Host").Value;
-			string fromEmail = config.GetSection("ConfigMail:FromEmail").Value;
-			string passwordEmail = config.GetSection("ConfigMail:Password").Value;
-			int port = Int32.Parse(config.GetSection("ConfigMail:Port").Value);
-			bool enableSsl = Boolean.Parse(config.GetSection("ConfigMail:EnableSsl").Value);
-			string domain = config.GetSection("Domain").Value;
-			string _filePath = Path.Combine(webRootPath, "Templates/ChangePasswordTH.htm");
-			string html = System.IO.File.ReadAllText(_filePath);
-
-			MailMessage mail = new MailMessage();
-			SmtpClient SmtpServer = new SmtpClient();
-			mail.To.Add(toMail);
-			mail.From = new MailAddress(fromEmail);
-			mail.Subject = "รหัสผ่านใหม่ของท่าน";
-			mail.IsBodyHtml = true;
-			mail.Body = string.Format(html, fullname, username, password, domain + "Login");
-			SmtpServer.Host = host;
-			SmtpServer.Port = port;
-			SmtpServer.EnableSsl = enableSsl;
-			SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-			SmtpServer.Send(mail);
-			return true;
-		}
-
-
-
-
 	}
 }
--- cisApp/Controllers/LoginController.cs
+++ cisApp/Controllers/LoginController.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 using cisApp.Function;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using cisApp.library;
+using Microsoft.AspNetCore.Hosting;
 
 namespace cisApp.Controllers
 {
     public class LoginController : BaseController
     {
+        private readonly IWebHostEnvironment _HostingEnvironment;
+        public LoginController(IWebHostEnvironment environment)
+        {
+            _HostingEnvironment = environment;
+        }
         public IActionResult Index()
         {
             return View(new LoginModel() { username= "admin@gmail.com", password="12345" });
@@ -24,7 +31,14 @@ namespace cisApp.Controllers
             {
                 if(obj.username == "admin@gmail.com" && obj.password == "12345")
                 {
-                    return LoginTest(new UserModel() { UserId= Guid.Parse("F71758CF-3F30-432B-8361-5754CDE8BF26"), Email= "admin@gmail.com", Fname= "ทดสอบ", Lname= "ระบบ", UserType=3 });
+                    return LoginTest(new UserModel() { 
+                        UserId= Guid.Parse("F71758CF-3F30-432B-8361-5754CDE8BF26")
+                        , Email= "admin@gmail.com"
+                        , Fname= "ทดสอบ"
+                        , Lname= "ระบบ"
+                        , UserType=3
+                        , RoleId = Guid.Parse("0A36BB48-C69B-4B3B-9E2E-F486918A5352")
+                    });;
                 }
                 if (string.IsNullOrEmpty(obj.username) || string.IsNullOrEmpty(obj.password))
                 {
@@ -61,6 +75,36 @@ namespace cisApp.Controllers
                 return Json(new ResponseModel().ResponseError(messageError));
             } 
         }
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult ForgetPassword(LoginModel obj)
+        {
+            try
+            {
+                var user = GetUser.Get.GetByEmail(obj.username);
+
+                if (user == null)
+                {
+                    return Json(new ResponseModel().ResponseError("ไม่พบชื่อผู้ใช้ดังกล่าวในระบบ"));
+                }
+
+                SendMail.SendMailResetPassword(user.Email, user.Email, "12345678", _HostingEnvironment.WebRootPath);
+
+                return Json(new ResponseModel().ResponseSuccess("ระบบได้ทำการส่งรหัสผ่านใหม่ไปยังอีเมลของท่านแล้ว", Url.Action("Index", "LogiN")));
+            }
+            catch (Exception ex)
+            {
+                string messageError = "พบข้อผิดพลาด กรุณาลองใหม่";
+                return Json(new ResponseModel().ResponseError(messageError));
+            }
+        }
+
 
         private void SetCookie(UserModel model)
         {
