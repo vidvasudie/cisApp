21e8dffb3273e246a105b2668d44fcb73e10825c
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2021-11-25 23:36:06 +0700

    add UI add Action add Card Job in Shared

--- cisApp/Controllers/JobsController.cs
+++ cisApp/Controllers/JobsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cisApp.Function;
+using cisApp.library;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,36 @@ using System.Threading.Tasks;
 
 namespace cisApp.Controllers
 {
-    public class JobsController : Controller
+    [CustomActionExecute("434737C0-D543-476A-A7CE-FBD6C4E155EC")]
+    public class JobsController : BaseController
     {
+        private Guid _PermissionMenuId;
+        private int _PermissionManage;
+        public JobsController()
+        {
+            _PermissionMenuId = Guid.Parse("434737C0-D543-476A-A7CE-FBD6C4E155EC");
+            _PermissionManage = 2;// สิทธิ์ผู้ใช้งาน
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public PartialViewResult ItemList(SearchModel model)
+        {
+            List<JobModel> _model = GetJobs.Get.GetJobs(model);
+            int count = GetJobs.Get.GetJobsTotal(model);
+
+            return PartialView("PT/_itemlist", new PaginatedList<JobModel>(_model, count, model.currentPage.Value, model.pageSize.Value));
+        }
+
+        public IActionResult Manage(SearchModel model)
+        {
+            List<JobModel> _model = GetJobs.Get.GetJobs(model);
+            if (_model != null)
+                return View(_model.FirstOrDefault());
+            return View(new JobModel());
         }
         public IActionResult Detail(string Mode)
         {
