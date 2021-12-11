60f76f57cf733f210cd4bdb4978039c8a2280c77
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2021-12-11 22:30:01 +0700

    เพิ่ม ดึง/แก้ไขข้อมูล Bank

---
+++ cisApp.API/Controllers/BankController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cisApp.API.Models;
+using cisApp.Function;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace cisApp.API.Controllers
+{ 
+    [ApiController]
+    public class BankController : BaseController
+    {
+        [Route("api/bank/profile")]
+        [HttpGet]
+        public IActionResult GetProfileBank(Guid userId)
+        {
+            try
+            {
+                if (userId == Guid.Empty)
+                {
+                    return BadRequest(resultJson.errors("parameter ไม่ถูกต้อง", "Invalid Request.", null));
+                }
+                var dsUser = GetUserDesigner.Get.GetBankProfile(userId);
+                if(dsUser == null)
+                {
+                    return Ok(resultJson.success("ไม่พบข้อมูล", "Data not found.", null));
+                }
+                 
+                return Ok(resultJson.success("ดึงข้อมูลสำเร็จ", "success", dsUser));
+            }
+            catch(Exception ex)
+            {
+                return Ok(resultJson.errors("ดึงข้อมูลไม่สำเร็จ", "fail", ex));
+            }
+        }
+
+        [Route("api/bank/update")]
+        [HttpPut]
+        public IActionResult GetUpdateBank([FromBody] BankModel model)
+        {
+            try
+            {
+                var data = GetUserDesigner.Manage.UpdateBankProfile(model);
+                if(data == null)
+                {
+                    return Ok(resultJson.errors("บันทึกข้อมูลไม่สำเร็จ", "fail", null));
+                }
+                var dsUser = GetUserDesigner.Get.GetBankProfile(model.UserId);
+                 
+                return Ok(resultJson.success("บันทึกข้อมูลสำเร็จ", "success", dsUser));
+            }
+            catch (Exception ex)
+            {
+                return Ok(resultJson.errors("บันทึกข้อมูลไม่สำเร็จ", "fail", ex));
+            }
+            
+        }
+    }
+}
---
+++ cisApp.Function/Models/BankModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cisApp
+{
+    public class BankModel
+    {
+        [Required]
+        public int BankId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public string AccountNumber { get; set; }
+        [Required]
+        public int AccountTypeId { get; set; }
+
+    }
+}
