9986c83e951ff553cce9bf905a2920f8f70454be
thitsaran <thitsaran.p@gmail.com>
2021-12-26 00:56:27 +0700

    push

---
+++ cisApp.API/SignalR/TestSignalR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace cisApp.API.SignalR
+{
+    public class TestSignalR : Hub
+    {
+        public async Task BoardcastMessage(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            await Clients.All.SendAsync("BoardcastMessage", name, message);
+        }
+    }
+}
--- cisApp.SignalRTest/Startup.cs
+++ cisApp.SignalRTest/Startup.cs
@@ -1,19 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks; 
-using System.IO;
-using Microsoft.OpenApi.Models;
-using cisApp.API.SignalR;
+using System.Threading.Tasks;
 
-namespace cisApp.API
+namespace cisApp.SignalRTest
 {
     public class Startup
     {
@@ -27,56 +23,40 @@ namespace cisApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            services.AddSignalR();
-
-            //services.AddCors(options =>
-            //    options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-            });
-
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseCors(builder => builder
-                //.AllowAnyOrigin()
+                .AllowAnyOrigin()
                 .AllowAnyMethod()
-                .AllowAnyHeader()
-                .SetIsOriginAllowed(origin => true)
-                .AllowCredentials());
+                .AllowAnyHeader());
 
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("v1/swagger.json", "MyAPI V1");
-            });
-
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
-
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapHub<TestSignalR>("/hubs/test");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
