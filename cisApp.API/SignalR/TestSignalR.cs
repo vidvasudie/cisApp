ddf29fe62f956588203a9ffee11a524d62658209
thitsaran <thitsaran.p@gmail.com>
2021-12-26 22:56:23 +0700

    เพิ่มการ map ทำให้สามารถส่งข้อมูลไปหา user ที่ต้องการได้

--- cisApp.API/SignalR/TestSignalR.cs
+++ cisApp.API/SignalR/TestSignalR.cs
@@ -8,10 +8,56 @@ namespace cisApp.API.SignalR
 {
     public class TestSignalR : Hub
     {
+        static List<UserAndConnectionId> _CurrentConnection = new List<UserAndConnectionId>();
+
+        public override Task OnConnectedAsync()
+        {
+            string userId = Context.GetHttpContext().Request.Query["userId"];
+
+            UserAndConnectionId newConnecition = new UserAndConnectionId()
+            {
+                User = userId,
+                ConnectionId = Context.ConnectionId
+            };
+
+            _CurrentConnection.Add(newConnecition);
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+
+            try
+            {
+                _CurrentConnection = _CurrentConnection.Where(o => o.ConnectionId != Context.ConnectionId).ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task BoardcastMessage(string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
-            await Clients.All.SendAsync("BoardcastMessage", name, message);
+
+            string messageString = "";
+            foreach(var item in _CurrentConnection)
+            {
+                messageString = messageString + "\n" + "conntectionid = " + item.ConnectionId + ", userid=" + item.User;
+            }
+
+            await Clients.All.SendAsync("BoardcastMessage", name, message + " - connections = " + messageString);
+            await Clients.Client(name).SendAsync("BoardcastMessage", name, "specific to client" + name);
+        }
+
+        public class UserAndConnectionId
+        {
+            public string User { get; set; }
+            public string ConnectionId { get; set; }
         }
     }
 }
