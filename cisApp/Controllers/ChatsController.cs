73af8c88a7a004561d5428c7f803386f48a26c56
thitsaran <thitsaran.p@gmail.com>
2021-12-30 01:27:48 +0700

    sync

--- cisApp/Controllers/ChatsController.cs
+++ cisApp/Controllers/ChatsController.cs
@@ -6,17 +6,58 @@ using System.Threading.Tasks;
 
 namespace cisApp.Controllers
 {
-    public class ChatsController : Controller
+    public class ChatsController : BaseController
     {
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Private()
         {
             return View();
         }
+
         public IActionResult Group()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult GetChatList(int type = 0) // 0 is private , 1 is group
+        {
+            try
+            {
+
+
+                return PartialView("PT/_ChatList");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public PartialViewResult GetChatCard(Guid? id)
+        {
+            try
+            {
+
+
+                return PartialView("PT/_ChatCard");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult XPrivate()
+        {
+            return View();
+        }
+        public IActionResult XGroup()
         {
             return View();
         }
---
+++ cisApp/wwwroot/js/pages/chat/index.js
@@ -0,0 +1,56 @@
+﻿
+function initView() {
+    loadChatList()
+}
+
+function loadChatList() {
+    CallAjax(
+        _ChatListUrl,
+        'GET',
+        null,
+        function (res) {
+            try {
+                console.log(res)
+
+                $('#chat-list').html(res)
+            }
+            catch (ex) {
+                console.log(ex)
+            }
+        },
+        function (err) {
+            try {
+                console.log(err)
+            }
+            catch (ex) {
+                console.log(ex)
+            }            
+        }
+    )
+}
+
+function loadChatCard() {
+    CallAjax(
+        _ChatListUrl,
+        'GET',
+        null,
+        function (res) {
+            try {
+                console.log(res)
+
+                $('#chat-list').html(res)
+            }
+            catch (ex) {
+                console.log(ex)
+            }
+        },
+        function (err) {
+            try {
+                console.log(err)
+            }
+            catch (ex) {
+                console.log(ex)
+            }
+        }
+    )
+}
