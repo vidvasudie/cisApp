67bf3bbdb4ba93199786512d4ec283683ee90150
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2022-02-23 20:43:10 +0700

    ปรับการคือค่า UrlPathAPI

--- cisApp.API/Controllers/DesignerFuncController.cs
+++ cisApp.API/Controllers/DesignerFuncController.cs
@@ -78,8 +78,8 @@ namespace cisApp.API.Controllers
                     o.UrlPathUserImage,
                     o.RecruitedPrice,
                     o.ContestPrice,
-                    JobCandidates = o.jobCandidates.Select(s => new { caUserId = s.UserId, caFullname = s.UserFullName, s.UrlPathAPI }),
-                    JobsExamImages = o.jobsExamImages.Select(s => new { s.UrlPathAPI })
+                    JobCandidates = o.jobCandidates.Select(s => new { caUserId = s.UserId, caFullname = s.UserFullName, UrlPathAPI= s.AttachFileId != Guid.Empty ? s.UrlPathAPI : null }),
+                    JobsExamImages = o.jobsExamImages.Select(s => new { UrlPathAPI=s.AttachFileId != Guid.Empty ? s.UrlPathAPI : null })
                 }).ToList(), model.take, jobs.Count()));
             }
             catch (Exception ex)
