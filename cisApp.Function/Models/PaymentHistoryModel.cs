fb15c399f975ea2a3c1db6847fd44a4952f32892
thitsaran <thitsaran.p@gmail.com>
2022-03-26 23:58:25 +0700

    วันที่บันทึกไม่ได้

--- cisApp.Function/Models/PaymentHistoryModel.cs
+++ cisApp.Function/Models/PaymentHistoryModel.cs
@@ -9,16 +9,6 @@ namespace cisApp.Function
     public class PaymentHistoryModel : PaymentHistory
     {
         public string FullName { get; set; }
-        public string PaymentDateStr
-        {
-            get
-            {
-                return PaymentDate.ToStringFormat();
-            }
-            set
-            {
-                PaymentDate = value.ToDateTimeFormat();
-            }
-        }
+        
     }
 }
