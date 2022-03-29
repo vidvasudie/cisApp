66a77457b46e0e36989439efbc0e41aa16b21557
chanwit faengkod <chanwit.f@hotmail.com>
2022-03-30 00:02:34 +0700

    update Noti

---
+++ cisApp.library/MobileNotfication.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DIGITAL_ID.library;
+using Newtonsoft.Json;
+
+namespace cisApp.library
+{
+    public   class MobileNotfication
+    {
+
+        #region สำหรับนักออกแบบ
+
+
+        public enum ModeDesigner
+        {
+            favorite, //เมื่อมีลูกค้าที่ กด ชอบไว้ สร้างใบงาน 
+            contest, // เมื่อได้รับเลือกในการเข้าประกวด
+            winner, // เมื่อได้รับการเลือกจากลูกค้า
+            submit, //เมื่อต้อง ถึงเวล่ใกล้ส่งงาน
+            alert  // เมื่อลูกค้าแจ้งขอแก้ไขงาน 
+        }
+
+        public async void Fordesigner(ModeDesigner modeDesigner, string clientID)
+        {
+            NotiModel obj = new NotiModel();
+            obj.to = clientID;
+            obj.notification = new notification();
+
+            switch (modeDesigner.ToString())
+            {
+                case "favorite":
+
+                    obj.notification.body = "มีใบงานที่กดถูกใจคุณ ถูกสร้างขึ้นคลิ๊กเลย";
+                    obj.notification.title = "มีลูกค้าสนใจคุณ!";
+                    //obj.notification.icon = "";
+
+                    break;
+                case "contest":
+
+                    obj.notification.body = "มีใบงานที่คุณได้รับคัดเลือกเข้าประกวดงาน อย่าลืมสอบถามรายละเอียดความต้องการละ คลิ๊กเลยเพื่อดูรายละเอียด ";
+                    obj.notification.title = "คุณได้รับคัดเลือกในกวดเข้าร่วมประกวดงาน";
+                    //obj.notification.icon = ""; 
+                    break;
+                case "winner":
+
+                    obj.notification.body = "เราขอแสดงความยินดีด้วยงานของคุณได้รับเลือกให้เป็นผู้ชนะในครั้งนี้ อย่าลืมส่งรายละเอียดการออกแบบให้ลูกค้า คลิ๊กเลยเพื่อดูรายละเอียด ";
+                    obj.notification.title = "ขอแสดงความยินดี";
+                    //obj.notification.icon = ""; 
+                    break;
+                case "submit":
+
+                    obj.notification.body = "เราแจ้งเตือนการส่งงานเนื่องจากงานที่ท่านได้ประกวดไว้ใกล้ถึงกำหนดส่ง คลิ๊กเลยเพื่อดูรายละเอียด";
+                    obj.notification.title = "ใกล้ถึงเวลาส่งงานแล้วนะ!";
+                    //obj.notification.icon = ""; 
+                    break;
+                case "alert":
+
+                    obj.notification.body = "เราขอแจ้งให้ท่านทราบว่า ใบงานของท่านได้ถูกขอแก้ไขโดยลูกค้า อย่าลืมสอบถามรายละเอียดความต้องการละ คลิ๊กเลยเพื่อดูรายละเอียด";
+                    obj.notification.title = "มีใบงานถูกขอแก้ไข!";
+                    //obj.notification.icon = ""; 
+                    break; 
+            }
+            await NotifyAsync(obj);
+        }
+
+
+
+
+
+
+
+        public class NotiModel
+        { 
+         public string to { get; set; } 
+        public notification notification { get; set; }
+        }
+        public class notification {
+            public string body { get; set; }
+            public string title { get; set; }
+            public string click_action { get; set; }
+
+
+        }
+
+
+
+
+
+        #endregion
+        #region สำหรับลูกค้า
+        public enum Modecustomer
+        {
+            regist,// เมื่อ มี ดีไซต์เนอร์สมัคร
+            regist3,// เมื่อมีดีไซต์เนอร์ครบ 3 คนแจ้งให้จ่ายเงิน
+            submit,// เมื่อ นักออกแบบส่งงาน
+        }
+        #endregion
+
+        public async void Forcustomer(Modecustomer modeDesigner, string clientID)
+        {
+
+
+
+            NotiModel obj = new NotiModel();
+            obj.to = clientID;
+            obj.notification = new notification();
+
+            switch (modeDesigner.ToString())
+            {
+                case "regist":
+
+                    obj.notification.body = "นักออกแบบบางท่านได้เข้ามา  คลิ๊กเลยเพื่อดูรายละเอียด";
+                    obj.notification.title = "มีนักออกแบบสนใจงานของคุณ";
+                    //obj.notification.icon = "";
+
+                    break;
+                case "regist3":
+
+                    obj.notification.body = "ใบงานที่ท่านได้ทำรายการไว้มีผู้สมัครครบเรียบร้อยแล้ว กรุณาเข้าสู่ขั้นตอนถัดไป คลิ๊กเลยเพื่อดูรายละเอียด ";
+                    obj.notification.title = "ใบงานของท่านมีผู้สมัครครบแล้ว";
+                    //obj.notification.icon = ""; 
+                    break;
+                case "submit":
+
+                    obj.notification.body = "เราขอแสดงความยินดีด้วยงานของคุณได้รับเลือกให้เป็นผู้ชนะในครั้งนี้ อย่าลืมส่งรายละเอียดการออกแบบให้ลูกค้า คลิ๊กเลยเพื่อดูรายละเอียด ";
+                    obj.notification.title = "ขอแสดงความยินดี";
+                    //obj.notification.icon = ""; 
+                    break;
+               
+            }
+            await NotifyAsync(obj);
+        }
+
+
+
+
+
+        public async Task<bool> NotifyAsync(NotiModel obj )
+        {
+            try
+            {
+                // Get the server key from FCM console
+                var serverKey = string.Format("key={0}", "AAAAqkbgFso:APA91bGczQPjYBPkej-7mhArSDDgeGGCK1pylhuSIvipTcIpt-BjRh4_7md4omhfK7g7tB7yJ6KdgtmEINpOniQ4DpCSmHy_zsaNRGZHDfkLb6QHGQMUgsX5_HPN4L3y7n0pMyu3X3wU");
+
+                // Get the sender id from FCM console
+               // var senderId = string.Format("id={0}", "477030036496");
+
+             
+
+                // Using Newtonsoft.Json
+             
+
+                using (var httpRequest = new HttpRequestMessage(HttpMethod.Post, "https://fcm.googleapis.com/fcm/send"))
+                {
+                    httpRequest.Headers.TryAddWithoutValidation("Authorization", serverKey);
+                 //   httpRequest.Headers.TryAddWithoutValidation("Sender", senderId);
+                    httpRequest.Content = new StringContent(JsonConvert.SerializeObject(obj) , Encoding.UTF8, "application/json");
+
+                    using (var httpClient = new HttpClient())
+                    {
+                        var result = await httpClient.SendAsync(httpRequest);
+
+                        if (result.IsSuccessStatusCode)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            // Use result.StatusCode to handle failure
+                            // Your custom error handler here
+                            //_logger.LogError($"Error sending notification. Status Code: {result.StatusCode}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Exception thrown in Notify Service: {ex}");
+            }
+
+            return false;
+        }
+
+
+    }
+}
