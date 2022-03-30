30faf5196d81105c9b88e373034046bb09f666b6
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2022-03-30 21:26:08 +0700

    ปรับการแสดงผลรูปภาพ

--- cisApp.Function/Models/FileAttachModel.cs
+++ cisApp.Function/Models/FileAttachModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using cisApp.Core;
+using Microsoft.Extensions.Configuration;
 
 namespace cisApp.Function
-{
+{ 
     public class FileAttachModel : AttachFile
     {
+        public IConfigurationRoot _config = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json")
+                                    .Build();
         public Guid gId { get; set; }
         public int Id { get; set; }
         public int TypeId { get; set; }
@@ -16,6 +22,10 @@ namespace cisApp.Function
         public int NextImg { get; set; }
         public int NextImgSelected { get; set; }
         public int col { get; set; }
+        public string AdminUrlBase 
+        { 
+            get { return _config.GetSection("WebConfig:AdminWebStie").Value; } 
+        }
         public string UrlPath2
         {
             get; set;
