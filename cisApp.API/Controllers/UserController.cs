bfaa60383468097fa2b164f642d1020038decc8d
thitsaran <thitsaran.p@gmail.com>
2022-05-26 20:28:00 +0700

    sync

--- cisApp.API/Controllers/UserController.cs
+++ cisApp.API/Controllers/UserController.cs
@@ -163,7 +163,9 @@ namespace cisApp.API.Controllers
                     return Unauthorized(resultJson.errors("ไม่พบข้อมูล", "ไม่พบข้อมูล", null));
                 }
                 var Obj = GetUser.Get.GetById(id.Value);
-                return Ok(resultJson.success(null, null, new { Obj.Fname, Obj.Lname, Obj.Tel, Obj.Email, Obj.LikeOtherCount, Obj.OtherLikedCount  }, null, null, null, null));
+
+                var bookmarkCount = GetUserBookmarkImage.Get.Total(id.Value);
+                return Ok(resultJson.success(null, null, new { Obj.Fname, Obj.Lname, Obj.Tel, Obj.Email, Obj.LikeOtherCount, Obj.OtherLikedCount, BookmarkCount = bookmarkCount }, null, null, null, null));
             }
             catch (Exception ex)
             {
