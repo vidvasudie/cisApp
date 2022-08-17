b1ff0ec87d41a909ff81d0b6efd694307bce860e
vidvasudie <42729863+vidvasudie@users.noreply.github.com>
2022-08-17 23:09:34 +0700

    ปรับเพิ่ม ตารางรรายงาน

--- cisApp/Controllers/ActivityLogsController.cs
+++ cisApp/Controllers/ActivityLogsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cisApp.Function;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,15 +7,36 @@ using System.Threading.Tasks;
 
 namespace cisApp.Controllers
 {
+    [CustomActionExecute("BF5D2740-6400-4D0F-9E75-8554B0E2D374")]
     public class ActivityLogsController : BaseController
     {
-        public IActionResult Index()
+        private Guid _PermissionMenuId;
+        private int _PermissionManage;
+        public ActivityLogsController()
+        {
+            _PermissionMenuId = Guid.Parse("BF5D2740-6400-4D0F-9E75-8554B0E2D374");
+            _PermissionManage = 2;// สิทธิ์ผู้ใช้งาน
+        } 
+
+        public IActionResult Index(SearchModel model)
         {
             if (_UserId() != null && _UserId() != Guid.Empty)
             {
                 LogActivityEvent(Common.LogCommon.LogMode.LOG);
             }
-            return View();
+            return View(model);
         }
+
+        [HttpPost]
+        public JsonResult Search(SearchModel model)
+        {
+            return Json(new ResponseModel().ResponseSuccess());
+        }
+
+        public PartialViewResult GetTable()
+        {  
+            return PartialView("PT/_itemlist");
+        }
+
     }
 }
